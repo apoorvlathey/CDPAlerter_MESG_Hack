7973a47c331a11f5284eb602ec1216cf0b6b29d3
CodinMaster <latheyapoorv@gmail.com>
2020-04-27 23:47:55 +0530

    added setupbot

---
+++ CDPAlerter_SetupBot/index.js
@@ -0,0 +1,83 @@
+const Telegraf = require("telegraf"); // import telegram lib
+const Markup = require("telegraf/markup");
+const Stage = require("telegraf/stage");
+const session = require("telegraf/session");
+const WizardScene = require("telegraf/scenes/wizard");
+const { enter, leave } = Stage
+const axios = require("axios")
+
+if(process.env.BOT_TOKEN === null){
+  require('dotenv').config();
+}
+
+
+const BOT_TOKEN = process.env.BOT_TOKEN || "";
+const URL = process.env.URL; // get the Heroku config var URL
+const PORT = process.env.PORT || 2000;
+const CHAT_ID = process.env.CHAT_ID;
+
+//delete previous instance
+axios.get(`https://api.telegram.org/bot${BOT_TOKEN}/deleteWebHook`);
+
+const bot = new Telegraf(BOT_TOKEN); // get the token from envirenment variable
+
+// Config the webhook for heroku
+// bot.telegram.setWebhook(`${URL}/bot${BOT_TOKEN}`);
+// bot.startWebhook(`/bot${BOT_TOKEN}`, null, PORT);
+
+// display Welcome text when we start bot
+bot.start((ctx) =>
+  bot.telegram.sendMessage(ctx.chat.id,
+    `Welcome to CDPAlerter!\n
+    ${ctx.from.first_name}, please Setup Alerts for your CDP Vault:\n
+    /setupalert`,
+    Markup.inlineKeyboard([
+      Markup.callbackButton("Setup Alert", "SETUP_ALERT"),
+    ]).extra()
+  )
+);
+
+const initializeVaultData = new WizardScene(
+  "initialize",
+  (ctx) => {
+    bot.telegram.sendMessage(ctx.chat.id, "Let's Setup Alerts for your Vault!\nEnter your Vault No. to track.");
+    return ctx.wizard.next();
+  },
+  (ctx) => {
+    /*
+     * ctx.wizard.state is the state management object which is persistent
+     * throughout the wizard
+     * we pass to it the previous user reply (supposed to be the source Currency )
+     * which is retrieved through `ctx.message.text`
+     */
+    ctx.wizard.state.vaultNo = ctx.message.text;
+    bot.telegram.sendMessage(ctx.chat.id, 
+      `Got it, enter the CDP Ratio to receive Alert for.\n[Eg: 200 for 200% limit]`
+    );
+    // Go to the following scene
+    return ctx.wizard.next();
+  },
+  (ctx) => {
+    const cdpLimit = (ctx.wizard.state.cdpLimit = ctx.message.text);
+    const vaultNo = ctx.wizard.state.vaultNo;
+
+    console.log("CDP limit:", cdpLimit, "VaultNo:", vaultNo);
+
+    bot.telegram.sendMessage(ctx.chat.id, 
+      `Done! You would get an alert for your Vault No. ${vaultNo} when the CDP goes below ${cdpLimit}%.`
+    );
+    return ctx.scene.leave();
+  }
+);
+// Scene registration
+const stage = new Stage([initializeVaultData], { ttl: 300 });
+bot.use(session());
+bot.use(stage.middleware());
+
+bot.command("setupalert", enter("initialize"));
+bot.action("SETUP_ALERT", enter("initialize"));
+bot.startPolling(); // start
+
+// bot.telegram.sendMessage(CHAT_ID, `Hello I am working`)
+
+console.log("Bot Started");
